24b8c505f848ae150734142473728b6e27612cf9
nicaf <florin.g.nica@gmail.com>
2017-04-24 15:46:55 +0300

    added react-router and a new route for adding candidate

--- cm-client/src/components/App.js
+++ cm-client/src/components/App.js
@@ -1,27 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import logo from '../img/logo.svg';
-import CandidatesTable from './CandidatesTable';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+
+import CandidatesTable from './CandidatesTable'
 
 import './App.css';
 
 injectTapEventPlugin();
 
 class App extends Component {
-  render() {
-    return (
-    <MuiThemeProvider>
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Candidate-Management</h2>
-        </div>
-        <CandidatesTable/>
-      </div>
-    </MuiThemeProvider>
-    );
-  }
+    render() {
+        return (
+            <MuiThemeProvider>
+                <div>
+                    <CandidatesTable/>
+                    <FloatingActionButton className="fab-button"
+                                          label="Add Candidate"
+                                          href="addCandidate">
+                        <ContentAdd/>
+                    </FloatingActionButton>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
