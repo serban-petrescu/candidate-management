acd3c8f63690e27add1a87f5b153f5aa6962c75b
tuddor <tuddor34@gmail.com>
2017-05-09 11:43:25 +0300

    adding sort by lastname and refactoring

--- cm-client/src/utils/sorting-comparators.js
+++ cm-client/src/utils/sorting-comparators.js
@@ -1,33 +1,67 @@
+/**
+ * Compares two candidates based on two attributes of the candidate.
+ * If the candidates are equal by the first criteria (attribute), the second criteria (attribute) will decide the order.
+ *
+ * @param c1 first candidate
+ * @param c2 second candidate
+ * @param order asc or desc
+ * @param primaryCriteria prioritary attribute of the candidate for sorting
+ * @param secondaryCriteria less prioritary attribute of the candidate for sorting
+ * @returns {number}
+ */
+function sortByTwoCriteria(c1, c2, order, primaryCriteria, secondaryCriteria) {
 
-function sortByLastName(c1, c2, order) {
     if (order === 'asc') {
-        if (c1.lastName > c2.lastName) {
+        if (c1[primaryCriteria] > c2[primaryCriteria]) {
             return 1;
         }
-        else if (c1.lastName < c2.lastName) {
+        else if (c1[primaryCriteria] < c2[primaryCriteria]) {
             return -1;
         }
         else {
-            if (c1.firstName > c2.firstName) {
-                return 1;
-            }
-            return -1;
+            // if primary criteria is equal
+            return c1[secondaryCriteria] > c2[secondaryCriteria] ? 1 : -1;
         }
     } else {
-        if (c1.lastName < c2.lastName) {
+        if (c1[primaryCriteria] < c2[primaryCriteria]) {
             return 1;
         }
-        else if (c1.lastName > c2.lastName) {
+        else if (c1[primaryCriteria] > c2[primaryCriteria]) {
             return -1;
         }
         else {
-            if (c1.firstName < c2.firstName) {
-                return 1;
-            }
-            return -1;
+            // if primary criteria is equal
+            return c1[secondaryCriteria] < c2[secondaryCriteria] ? 1 : -1;
         }
     }
 }
 
 
-export {sortByLastName}
+/**
+ * Sorting comparator for lastName of the candidate.
+ * If two candidates have the same lastName, they'll be compared by firstName
+ *
+ * @param c1 firstCandidate
+ * @param c2 secondCandidate
+ * @param order asc or desc
+ * @returns {number}
+ */
+function sortByLastName(c1, c2, order) {
+    return sortByTwoCriteria(c1, c2, order, "lastName", "firstName");
+}
+
+/**
+ * Sorting comparator for firstName of the candidate.
+ * If two candidates have the same firstName, they'll be compared by lastName
+ *
+ * @param c1 first candidate
+ * @param c2 second candidate
+ * @param order asc or desc
+ * @returns {number}
+ */
+function sortByFirstName(c1, c2, order) {
+    return sortByTwoCriteria(c1, c2, order, "firstName", "lastName");
+}
+
+
+export {sortByLastName, sortByFirstName};
