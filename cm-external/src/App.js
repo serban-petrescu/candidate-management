20256bba85a6189751f8fe59971c8b76736e966d
blajv <marcoblaj@gmail.com>
2017-06-28 17:25:00 +0300

    Initial commit

--- cm-external/src/App.js
+++ cm-external/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Recaptcha from "./Components/Recaptcha";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <Recaptcha/>
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
