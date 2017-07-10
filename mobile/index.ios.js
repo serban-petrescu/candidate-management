04bfb677907bd3ccd3575a8d3bbc1ebd514b6c00
oana-andreea <marin.oana.andreea>
2017-07-10 23:03:10 +0300

    react native set state

--- mobile/index.ios.js
+++ mobile/index.ios.js
@@ -17,12 +17,12 @@ export default class mobile extends Component {
     constructor(props){
         super(props);
         this.state = {
-            lastName: 'Popescu',
-            firstName: 'Maria',
-            email:'test@gmail.com',
-            phone:'0712345678',
-            studyYear:'3',
-            education:'UTCN-AC eng'
+            lastName: '',
+            firstName: '',
+            email:'',
+            phone:'',
+            studyYear:'',
+            education:''
 
         };
 
@@ -36,26 +36,26 @@ export default class mobile extends Component {
             email: this.state.email,
             phone: this.state.phone
         };
-        console.log(candidate);
         addCandidate(candidate);
         return 'trueee';
     };
+
 
     render() {
     return (
         <View style={{backgroundColor:'red'}}>
             <Text>Last Name</Text>
-            <TextInput style={{ backgroundColor: '#ededed', height: 60 }} value={this.state.lastName}/>
+            <TextInput style={{ backgroundColor: '#ededed', height: 60 }} onChangeText={(lastName) => this.setState({lastName})} placeholder='Popescu' value={this.state.lastName} />
             <Text>First Name</Text>
-            <TextInput style={{ backgroundColor: '#ededed', height: 60 }} value={this.state.firstName}/>
+            <TextInput style={{ backgroundColor: '#ededed', height: 60 }} onChangeText={(firstName) => this.setState({firstName})} placeholder='Maria' value={this.state.firstName}/>
             <Text>Email</Text>
-            <TextInput style={{ backgroundColor: '#ededed', height: 60 }} value={this.state.email}/>
+            <TextInput style={{ backgroundColor: '#ededed', height: 60 }} onChangeText={(email) => this.setState({email})} placeholder='test@gmail.com' value={this.state.email}/>
             <Text>Telefon</Text>
-            <TextInput style={{ backgroundColor: '#ededed', height: 60 }} value={this.state.phone}/>
+            <TextInput style={{ backgroundColor: '#ededed', height: 60 }} onChangeText={(phone) => this.setState({phone})} placeholder='0712345678' value={this.state.phone}/>
             <Text>Education</Text>
-            <TextInput style={{ backgroundColor: '#ededed', height: 60 }} value={this.state.education}/>
+            <TextInput style={{ backgroundColor: '#ededed', height: 60 }} onChangeText={(education) => this.setState({education})} placeholder='UTCN-AC eng' value={this.state.education}/>
             <Text>Study Year</Text>
-            <TextInput style={{ backgroundColor: '#ededed', height: 60 }} value={this.state.studyYear}/>
+            <TextInput style={{ backgroundColor: '#ededed', height: 60 }} onChangeText={(studyYear) => this.setState({studyYear})} placeholder='3' value={this.state.studyYear}/>
             <Button title="Sumbit" onPress={this.submitCandidate} />
         </View>
     );
