22964076744df6add4a340bca0645e23f8fbdbe2
Robert Zbiera <robert.zbiera@msg.group>
2018-01-23 12:33:22 +0200

    Updated CandidateResourceProcessor to set DateOfAdding as new Date() to not give NullPointerException in CandidateUtils line 98

--- cm-server/src/main/java/ro/msg/cm/processor/CandidateResourceProcessor.java
+++ cm-server/src/main/java/ro/msg/cm/processor/CandidateResourceProcessor.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Component;
 import ro.msg.cm.model.Candidate;
 import ro.msg.cm.util.CandidateUtils;
 
+import java.util.Date;
+
 @Component
 public class CandidateResourceProcessor implements ResourceProcessor<Resource<Candidate>> {
     private final CandidateUtils utils;
@@ -18,6 +20,7 @@ public class CandidateResourceProcessor implements ResourceProcessor<Resource<Ca
     public Resource<Candidate> process(Resource<Candidate> resource) {
         Candidate candidate = resource.getContent();
         if (candidate != null) {
+            candidate.setDateOfAdding(new Date());
             candidate.setCurrentStudyYear(utils.determineYearBasedOnDuration(candidate));
         }
         return resource;
