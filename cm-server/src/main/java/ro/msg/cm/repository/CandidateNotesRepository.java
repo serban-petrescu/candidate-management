d1bd870bd563d25c9dbdc8a2228fba990f1a9937
Robert Zbiera <robert.zbiera@msg.group>
2018-02-22 11:57:26 +0200

    Created REST Controllers for CandidateSkills, CandidateNotes, Education, Tag

--- cm-server/src/main/java/ro/msg/cm/repository/CandidateNotesRepository.java
+++ cm-server/src/main/java/ro/msg/cm/repository/CandidateNotesRepository.java
@@ -3,5 +3,12 @@ package ro.msg.cm.repository;
 import org.springframework.data.repository.CrudRepository;
 import ro.msg.cm.model.CandidateNotes;
 
+import java.util.List;
+
 public interface CandidateNotesRepository extends CrudRepository<CandidateNotes, Long> {
+
+    List<CandidateNotes> findAll();
+
+    List<CandidateNotes> save(List<CandidateNotes> candidateNotesList);
+
 }
--- cm-server/src/main/java/ro/msg/cm/repository/EducationRepository.java
+++ cm-server/src/main/java/ro/msg/cm/repository/EducationRepository.java
@@ -3,5 +3,12 @@ package ro.msg.cm.repository;
 import org.springframework.data.repository.CrudRepository;
 import ro.msg.cm.model.Education;
 
+import java.util.List;
+
 public interface EducationRepository extends CrudRepository<Education, Long> {
+
+    List<Education> findAll();
+
+    List<Education> save(List<Education> educationList);
+
 }
