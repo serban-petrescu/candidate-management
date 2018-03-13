8f71cac50a76dc4055341b07aa216ec161b6b6b7
RobbDora <robb.dora@gmail.com>
2018-03-13 12:18:29 +0000

    small fix

--- cm-server/src/main/java/ro/msg/cm/exception/CandidateIsAlreadyValidatedException.java
+++ cm-server/src/main/java/ro/msg/cm/exception/CandidateIsAlreadyValidatedException.java
@@ -2,7 +2,4 @@ package ro.msg.cm.exception;
 
 public class CandidateIsAlreadyValidatedException extends RuntimeException {
 
-    public CandidateIsAlreadyValidatedException() {
-        super();
-    }
 }
