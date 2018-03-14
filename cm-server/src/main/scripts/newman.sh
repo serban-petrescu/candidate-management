a8901c23870f621dd6372d7fd82206d7ebfb46cd
RobbDora <robb.dora@gmail.com>
2018-03-14 15:43:08 +0000

    small update

--- cm-server/src/main/scripts/newman.sh
+++ cm-server/src/main/scripts/newman.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-cd $HOME
-ls | grep newman
+cd $HOME/.cache
 node node_modules/newman/bin/newman.js run cm-server/src/test/resources/postman/candidate-management-tests.postman_collection.json
