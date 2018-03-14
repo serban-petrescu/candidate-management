4483d41ea4c801f3e6eed9a8dff33ab2616cc1d9
RobbDora <robb.dora@gmail.com>
2018-03-14 15:20:58 +0000

    small fix on bash script

--- cm-server/src/main/scripts/newman.sh
+++ cm-server/src/main/scripts/newman.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find -name node_modules
-node node_modules/newman/bin/newman.js run cm-server/src/test/resources/postman/candidate-management-tests.postman_collection.json
+cd $HOME/node_modules/
+node newman/bin/newman.js run cm-server/src/test/resources/postman/candidate-management-tests.postman_collection.json
