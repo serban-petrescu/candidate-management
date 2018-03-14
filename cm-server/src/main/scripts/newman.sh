6677def7763057e3fcd8867b84436264d12c1515
RobbDora <robb.dora@gmail.com>
2018-03-14 15:07:47 +0000

    updated path for newman file

--- cm-server/src/main/scripts/newman.sh
+++ cm-server/src/main/scripts/newman.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd $HOME/.m2/
-ls
-node node_modules/newman/bin/newman.js run cm-server/src/test/resources/postman/candidate-management-tests.postman_collection.json
+node repository/node_modules/newman/bin/newman.js run cm-server/src/test/resources/postman/candidate-management-tests.postman_collection.json
