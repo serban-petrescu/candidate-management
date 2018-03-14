52765cba48099f872bb37674bfc5b9724d79cc46
RobbDora <robb.dora@gmail.com>
2018-03-14 15:18:05 +0000

    command to find the node_modules folder

--- cm-server/src/main/scripts/newman.sh
+++ cm-server/src/main/scripts/newman.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-cd $HOME/.m2/repository
-ls
+find -name node_modules
 node node_modules/newman/bin/newman.js run cm-server/src/test/resources/postman/candidate-management-tests.postman_collection.json
