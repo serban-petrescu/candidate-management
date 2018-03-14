fdf9698d4cf25dca794fed909ab39af010bd123b
RobbDora <robb.dora@gmail.com>
2018-03-14 15:12:26 +0000

    modified newman.sh

--- cm-server/src/main/scripts/newman.sh
+++ cm-server/src/main/scripts/newman.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd $HOME/.m2/
-node repository/node_modules/newman/bin/newman.js run cm-server/src/test/resources/postman/candidate-management-tests.postman_collection.json
+cd $HOME/.m2/repository
+ls
+node node_modules/newman/bin/newman.js run cm-server/src/test/resources/postman/candidate-management-tests.postman_collection.json
