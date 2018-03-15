c7060089fde569668534ca1baae81d83eed252ea
paulcosma97 <mct0mit23m>
2018-03-15 17:22:49 +0200

    handled EntityNotFoundException and improved code

--- cm-server/src/main/java/ro/msg/cm/handler/ServiceResponseExceptionHandler.java
+++ cm-server/src/main/java/ro/msg/cm/handler/ServiceResponseExceptionHandler.java
@@ -15,6 +15,7 @@ import ro.msg.cm.exception.CandidateIsAlreadyValidatedException;
 import ro.msg.cm.exception.CandidateNotFoundException;
 import ro.msg.cm.exception.PatchCandidateInvalidValueException;
 
+import javax.persistence.EntityNotFoundException;
 import javax.validation.ConstraintViolationException;
 import java.util.HashMap;
 import java.util.Map;
@@ -23,6 +24,7 @@ import java.util.Map;
 @ControllerAdvice
 public class ServiceResponseExceptionHandler extends ResponseEntityExceptionHandler {
     private final static HttpHeaders HEADERS;
+    private final static String MESSAGE = "message";
 
     static {
         HEADERS = new HttpHeaders();
@@ -34,9 +36,9 @@ public class ServiceResponseExceptionHandler extends ResponseEntityExceptionHand
         Map<String, String> output = new HashMap<>();
 
         if (StringUtils.isEmpty(ex.getMessage())) {
-            output.put("message", "Candidate for the given id was not found");
+            output.put(MESSAGE, "Candidate for the given id was not found");
         } else {
-            output.put("message", ex.getMessage());
+            output.put(MESSAGE, ex.getMessage());
         }
 
         return handleExceptionInternal(ex, new Gson().toJson(output), HEADERS, HttpStatus.NOT_FOUND, request);
@@ -45,23 +47,15 @@ public class ServiceResponseExceptionHandler extends ResponseEntityExceptionHand
     @ExceptionHandler(value = CandidateIsAlreadyValidatedException.class)
     protected ResponseEntity<Object> handleCandidateIsAlreadyValidatedException(RuntimeException ex, WebRequest request) {
         Map<String, String> output = new HashMap<>();
-        output.put("message", "Invalidated candidate for the given id doesn't exist");
+        output.put(MESSAGE, "Invalidated candidate for the given id doesn't exist");
 
         return handleExceptionInternal(ex, new Gson().toJson(output), HEADERS, HttpStatus.NOT_FOUND, request);
     }
 
-    @ExceptionHandler(value = PatchCandidateInvalidValueException.class)
+    @ExceptionHandler(value = { PatchCandidateInvalidValueException.class, ConstraintViolationException.class })
     protected ResponseEntity<Object> handlePatchCandidateInvalidValueException(RuntimeException ex, WebRequest request) {
         Map<String, String> output = new HashMap<>();
-        output.put("message", "Sent candidate email or phone is not valid");
-
-        return handleExceptionInternal(ex, new Gson().toJson(output), HEADERS, HttpStatus.BAD_REQUEST, request);
-    }
-
-    @ExceptionHandler(value = ConstraintViolationException.class)
-    protected ResponseEntity<Object> handleConstraintViolationException(RuntimeException ex, WebRequest request) {
-        Map<String, String> output = new HashMap<>();
-        output.put("message", "Sent candidate email or phone is not valid");
+        output.put(MESSAGE, "Sent candidate email or phone is not valid");
 
         return handleExceptionInternal(ex, new Gson().toJson(output), HEADERS, HttpStatus.BAD_REQUEST, request);
     }
@@ -69,8 +63,16 @@ public class ServiceResponseExceptionHandler extends ResponseEntityExceptionHand
     @ExceptionHandler(value = CannotCreateTransactionException.class)
     protected ResponseEntity<Object> handleCannotCreateTransactionException(RuntimeException ex, WebRequest request) {
         Map<String, String> output = new HashMap<>();
-        output.put("message", "Database is offline or unreachable");
+        output.put(MESSAGE, "Database is offline or unreachable");
 
         return handleExceptionInternal(ex, new Gson().toJson(output), HEADERS, HttpStatus.SERVICE_UNAVAILABLE, request);
+    }
+
+    @ExceptionHandler(value = EntityNotFoundException.class)
+    protected ResponseEntity<Object> handleEntityNotFoundException(RuntimeException ex, WebRequest request) {
+        Map<String, String> output = new HashMap<>();
+        output.put(MESSAGE, "Entity could not be found");
+
+        return handleExceptionInternal(ex, new Gson().toJson(output), HEADERS, HttpStatus.NOT_FOUND, request);
     }
 }
