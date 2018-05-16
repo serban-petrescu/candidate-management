3392a115d1509f32c401bbfe16c495b5c678ed38
oana-andreea <marin.oana.andreea@gmail.com>
2018-05-16 14:39:40 +0300

    cleanup & mobile icon

--- mobile/android/app/src/main/java/com/mobile/MainApplication.java
+++ mobile/android/app/src/main/java/com/mobile/MainApplication.java
@@ -3,10 +3,7 @@ package com.mobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import io.realm.react.RealmReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
-import com.remobile.toast.RCTToastPackage;
-import com.chirag.RNMail.RNMail;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -27,10 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RealmReactPackage(),
-            new VectorIconsPackage(),
-            new RCTToastPackage(),
-            new RNMail()
+            new VectorIconsPackage()
       );
     }
   };
