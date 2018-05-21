94ea69cb071d701a743cccbf5e458f66bb323cdc
LorenaLore <lorena_fodor@yahoo.com>
2018-05-21 11:21:34 +0300

    #42 login logout UI - login reducer not needed => removed

--- cm-client/src/main/resources/reducers/index.js
+++ cm-client/src/main/resources/reducers/index.js
@@ -3,7 +3,6 @@ import CandidatesReducer from './reducer_candidates';
 import ActiveCandidateReducer from './reducer_active_candidate';
 import CandidateValidationReducer from './reducer_validation';
 
-import LoginReducer from './reducer_login';
 /**
  * This reducer calls every child reducer, and gathers their results into a single
  * state object. Each reducer manages an independent part of the state.
@@ -13,7 +12,6 @@ import LoginReducer from './reducer_login';
 const rootReducer = combineReducers({
     candidates: CandidatesReducer,
     activeCandidate: ActiveCandidateReducer,
-    loggedUser:LoginReducer,
     candidatesNYValidated: CandidateValidationReducer
 });
 
